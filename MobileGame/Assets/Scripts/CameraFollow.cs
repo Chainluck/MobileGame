82597a47e72d734fa2e7be0cec3e52aaeef65e85
Chainluck <39699234+Chainluck@users.noreply.github.com>
2019-05-16 18:26:58 +0300

    Camera LookAtPlayer
    Uploaded camera to look at player

---
+++ MobileGame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    private Vector3 _cameraOffset;
+    public float SmoothFactor = 0.5f;
+    public bool LookAtPlayer = false;
+
+
+    // called once when frame starts
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPosition, SmoothFactor);
+
+        if (LookAtPlayer)
+        {
+            transform.LookAt(PlayerTransform);
+        }
+    }
+}
