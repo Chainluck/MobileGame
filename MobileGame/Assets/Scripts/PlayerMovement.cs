8239eb63ce95dea6337bec55a4f29ec682a46bb9
Chainluck <39699234+Chainluck@users.noreply.github.com>
2019-05-16 18:58:52 +0300

    Test

--- MobileGame/Assets/Scripts/PlayerMovement.cs
+++ MobileGame/Assets/Scripts/PlayerMovement.cs
@@ -16,7 +16,6 @@ public class PlayerMovement : MonoBehaviour
             if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
             {
                 Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
-
                 transform.position = Vector3.Lerp(transform.position, touchPosition, Time.deltaTime);
             }
         }
