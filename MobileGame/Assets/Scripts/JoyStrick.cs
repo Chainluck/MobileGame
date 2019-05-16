6b01f9f3f538ad3dfed8ce43e085762a8459843f
Chainluck <39699234+Chainluck@users.noreply.github.com>
2019-05-16 20:33:45 +0300

    Joystrick
    Added joystrick to move cube

---
+++ MobileGame/Assets/Scripts/JoyStrick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStrick : MonoBehaviour
+{
+    protected Joystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.velocity = new Vector3(joystick.Horizontal * 50f + Input.GetAxis("Horizontal") * 50f, rigidbody.velocity.y, joystick.Vertical * 50f + Input.GetAxis("Vertical") * 50f);
+    }
+}
